b11e77b321e2675c7c47aa0d42dcc716b0f6d369
aaron-ang <67321817+aaron-ang@users.noreply.github.com>
2023-12-07 15:23:50 -0500

    simple angular with mock data

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import mockData from '../data/mockResponse.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-app';
+  data: any = [];
+
+  getData() {
+    this.data = mockData;
+  }
+
+  clearData() {
+    this.data = [];
+  }
 }
